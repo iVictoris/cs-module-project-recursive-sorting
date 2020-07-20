ca26184044f67e3c64e1330633ae97d52ec018a1
Victor <victor.tran@tempestas.io>
2020-07-19 22:13:41 -0400

    implement recursive binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,20 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
+def binary_search(arr:list, target, start, end):
     # Your code here
+    middle = start + end // 2
+
+    if len(arr) == 0 or len(arr) == 1 and target != arr[middle]:
+        return -1
+
+    if target == arr[middle]:
+        return middle
+
+    target_is_less_than_middle = target < arr[middle]
+    start = start if target_is_less_than_middle else middle + 1
+    end = middle - 1 if target_is_less_than_middle else end
+
+    return binary_search(arr, target, start, end)
+
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +25,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
